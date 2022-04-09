a08b12888a906eb577e4d168ffb9c8943719e079
Fatoş Dilara Fırtına <dilara_firtina@hotmail.com>
2022-04-09 18:02:35 +0300

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution in this file!
+const employee = {
+    name: "Dilara",
+    streetAddress : "Gürsu"
+}
+
+const updateEmployeeWithKeyAndValue = (employee, key, value) => {
+    let obj = {...employee}
+    obj[key] = value
+    return obj
+}
+
+updateEmployeeWithKeyAndValue(employee, "surname", "Fırtına");
+
+
+const destructivelyUpdateEmployeeWithKeyAndValue = (obj, key, value) => {
+    obj[key] = value
+    return obj
+}
+
+destructivelyUpdateEmployeeWithKeyAndValue(employee, "surname", "Fırtına");
+
+const deleteFromEmployeeByKey = (employee, key) => {
+    let obj = {...employee}
+    delete obj[key]
+    console.log(obj)
+    return obj
+}
+
+deleteFromEmployeeByKey(employee, "name");
+
+
+const destructivelyDeleteFromEmployeeByKey = (obj, key) => {
+   delete obj[key]
+    return obj
+}
+
+
+destructivelyDeleteFromEmployeeByKey(employee, "name");
